60cd42990056fdbdba9b59e29f85d9ec1527c223
kaunnikov <kaunnikov@gmail.com>
2022-04-20 21:04:58 +0500

    fix operations

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,18 +1,18 @@
 import random
 import operator
 
-operations = {
-    '+': operator.add,
-    '-': operator.sub,
-    '*': operator.mul,
-}
-
 
 def get_description():
     return 'What is the result of the expression?'
 
 
 def get_question_and_answer():
+    operations = {
+        '+': operator.add,
+        '-': operator.sub,
+        '*': operator.mul,
+    }
+
     operand1 = random.randint(0, 100)
     operand2 = random.randint(0, 100)
     operation = random.choice(list(operations.keys()))
